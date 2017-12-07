ed62da09916f69d20efb5a4076c53d9237f568bb
Andre Eisenbach <eisenbach@google.com>
2017-12-06 16:16:36 -0800

    axon7: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    Merged-In: I9655a14e021024540cf342022cb817e1f8d0f9ad
    Change-Id: I9655a14e021024540cf342022cb817e1f8d0f9ad

--- device.mk
+++ device.mk
@@ -293,9 +293,8 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
 PRODUCT_PACKAGES += \
