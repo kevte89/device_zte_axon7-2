cd789fd6d969888f41a7c2146649034be4703a6c
Ramjee Singh <ramjee@codeaurora.org>
2018-09-18 12:40:44 +0530

    axon7: ST HAL upgrade for P compatibility
    -Sound trigger HIDL interface supports loading of more
    than 1MB sound models from 2.1 version. Update to use HIDL
    version 2.1.
    
    Change-Id: I564f598f41f20a01ba9e8dd69be125097a134f1c

--- device.mk
+++ device.mk
@@ -97,8 +97,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
     audio_amplifier.msm8996 \
     audio.primary.msm8996 \
